ac772b356fa37452bacca44ff09226aed5af2be0
SB Designs <hello@trackle.serv00.net>
2024-11-21 06:52:14 +0000

    Update main.js

--- tube/main.js
+++ tube/main.js
@@ -25,7 +25,7 @@ class App {
       this.setMapTheme(); // Update map theme on device theme change
     });
 
-    let foundPlaces = JSON.parse(window.localStorage.getItem("places-dlr") || "{}");
+    let foundPlaces = JSON.parse(window.localStorage.getItem("places-tube") || "{}");
     this.placeList.forEach((place) => {
       if (foundPlaces[place.name]) {
         place.showOverlay();
@@ -46,7 +46,7 @@ class App {
 
     const mapStyle = darkMode
       ? "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2FpbGFuIiwiYSI6ImNreHh6MjNtNzJhd3oyb21wYjRkY2U0aGsifQ.tZzQ-GAom5_D8SLwrqmy-Q"
-      : "https://api.mapbox.com/styles/v1/kailan/clnmjy811006901qpergtfrho/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2FpbGFuIiwiYSI6ImNreHh6MjNtNzJhd3oyb21wYjRkY2U0aGsifQ.tZzQ-GAom5_D8SLwrqmy-Q";
+      : "https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2FpbGFuIiwiYSI6ImNreHh6MjNtNzJhd3oyb21wYjRkY2U0aGsifQ.tZzQ-GAom5_D8SLwrqmy-Q";
 
     // Remove existing tiles if they exist
     if (tiles) {
